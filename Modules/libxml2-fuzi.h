f39d0ac5e80641cb949d1eae935aff6aac596d04
Adrian Schoenig <adrian@schoenig.me>
2026-02-05 10:38:21 +1100

    Attempt to fix module map

--- Modules/libxml2-fuzi.h
+++ Modules/libxml2-fuzi.h
@@ -1,31 +1,17 @@
 #ifndef LIBXML2_FUZI_H
 #define LIBXML2_FUZI_H
-#include <libxml2/libxml/tree.h>
-#include <libxml2/libxml/xmlreader.h>
-#include <libxml2/libxml/xpath.h>
-#include <libxml2/libxml/xpathInternals.h>
-#include <libxml2/libxml/HTMLtree.h>
-#include <libxml2/libxml/HTMLparser.h>
-#include <libxml2/libxml/parser.h>
-#include <libxml2/libxml/entities.h>
-#include <libxml2/libxml/SAX.h>
-#include <libxml2/libxml/SAX2.h>
+#include <libxml/tree.h>
+#include <libxml/xmlreader.h>
+#include <libxml/xpath.h>
+#include <libxml/xpathInternals.h>
+#include <libxml/HTMLtree.h>
+#include <libxml/HTMLparser.h>
+#include <libxml/parser.h>
+#include <libxml/entities.h>
+#include <libxml/SAX.h>
+#include <libxml/SAX2.h>
 
-#if defined(_WIN32)
-# if defined(LIBXML_STATIC)
-#   if defined(LIBXML_DEBUG)
-#     pragma comment(lib, "libxml2sd.lib")
-#   else
-#     pragma comment(lib, "libxml2s.lib")
-#   endif
-# else
-#   if defined(LIBXML_DEBUG)
-#     pragma comment(lib, "xml2d.lib")
-#   else
-#     pragma comment(lib, "xml2.lib")
-#   endif
-# endif
-#elif defined(__ELF__)
+#if defined(__ELF__)
 __asm__ (".section .swift1_autolink_entries,\"a\",@progbits\n"
          ".p2align 3\n"
          ".L_swift1_autolink_entries:\n"
